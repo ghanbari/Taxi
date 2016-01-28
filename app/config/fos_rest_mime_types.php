6ef8da2b8004d6bd935cf865c6bfbf2ae4194ccd
Ghanbari <ghanbari.daniel@gmail.com>
2016-01-28 10:35:36 +0330

    Config rest service

---
+++ app/config/fos_rest_mime_types.php
@@ -0,0 +1,26 @@
+<?php
+
+$versions = array("1.0.0");
+
+$mimeTypes = array(
+    'json' => array(
+        'application/json',
+    ),
+);
+
+array_walk($mimeTypes, function (&$mimeTypes, $format, $versions) {
+    $versionMimeTypes = array();
+    foreach ($mimeTypes as $mimeType) {
+        foreach ($versions as $version) {
+            array_push($versionMimeTypes, sprintf('%s;version=%s', $mimeType, $version));
+            array_push($versionMimeTypes, sprintf('%s;v=%s', $mimeType, $version));
+        }
+    }
+    $mimeTypes = array_merge($mimeTypes, $versionMimeTypes);
+}, $versions);
+
+$container->loadFromExtension('fos_rest', array(
+    'view' => array(
+        'mime_types' => $mimeTypes,
+    ),
+));
