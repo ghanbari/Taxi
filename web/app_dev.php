441f14aed14705c615e0d347a6cbe3376566908c
Ghanbari <ghanbari.daniel@gmail.com>
2016-01-28 10:53:13 +0330

    Add secret cookie

--- web/app_dev.php
+++ web/app_dev.php
@@ -8,11 +8,14 @@ use Symfony\Component\Debug\Debug;
 // for more information
 //umask(0000);
 
+$secrets = array('MGH09Ra34Qiq05pr1Wf2j3TCw3a19CV43eB', 'SADnC820pfCo9LK622zOjXF615c27OlrT85', 'ANONu7K67Ut18Mq4ldvLKg98INjZeL420z8I');
+
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')) || php_sapi_name() === 'cli-server')
+    || (isset($_COOKIE['secret']) and in_array($_COOKIE['secret'], $secrets))
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
