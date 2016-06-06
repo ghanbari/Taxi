e92b09f6d0f4bcdb835bee6ca69d4c4d31bdcdd5
Ghanbari <ghanbari.daniel@gmail.com>
2016-06-06 11:13:04 +0430

    fix: status code for car status Exception

--- src/FunPro/DriverBundle/Exception/RuntimeCarStatusException.php
+++ src/FunPro/DriverBundle/Exception/RuntimeCarStatusException.php
@@ -4,10 +4,12 @@ namespace FunPro\DriverBundle\Exception;
 
 class RuntimeCarStatusException extends \RuntimeException
 {
-    protected $code = 1;
+    protected $code = -1;
 
     public function __construct($message, $file='', $line='')
     {
+        parent::__construct($message, 400);
+
         $this->message = $message;
         if (!empty($file)) {
             $this->file = $file;
