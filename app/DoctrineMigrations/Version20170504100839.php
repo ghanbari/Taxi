dcaf034ff1178395b51b3323d6234bb269e3180f
Ghanbari <ghanbari.daniel@gmail.com>
2017-05-05 11:40:22 +0430

    fix: born must have default value

--- app/DoctrineMigrations/Version20170504100839.php
+++ app/DoctrineMigrations/Version20170504100839.php
@@ -18,7 +18,7 @@ class Version20170504100839 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE driver ADD parent_name VARCHAR(50) NOT NULL, ADD born DATE NOT NULL, ADD education SMALLINT NOT NULL, ADD cod_status SMALLINT NOT NULL, ADD is_marriage TINYINT(1) NOT NULL, ADD sheba_number VARCHAR(20) NOT NULL, ADD start_activity DATE NOT NULL, ADD end_activity DATE NOT NULL, ADD learning_course TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE driver ADD parent_name VARCHAR(50) NOT NULL, ADD born DATE DEFAULT \'2017/01/01\' NOT NULL, ADD education SMALLINT NOT NULL, ADD cod_status SMALLINT NOT NULL, ADD is_marriage TINYINT(1) NOT NULL, ADD sheba_number VARCHAR(20) NOT NULL, ADD start_activity DATE NOT NULL, ADD end_activity DATE NOT NULL, ADD learning_course TINYINT(1) NOT NULL');
     }
 
     /**
