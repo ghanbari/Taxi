91db39dc39fce0ae3be1d464bfab14ab7a6d879d
Ghanbari <ghanbari.daniel@gmail.com>
2017-06-02 16:56:33 +0430

    Define default location

--- src/FunPro/FinancialBundle/DataFixtures/ORM/LoadBaseCostData.php
+++ src/FunPro/FinancialBundle/DataFixtures/ORM/LoadBaseCostData.php
@@ -5,6 +5,7 @@ namespace FunPro\FinancialBundle\DataFixtures\ORM;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use FunPro\FinancialBundle\Entity\BaseCost;
+use FunPro\GeoBundle\Doctrine\ValueObject\Point;
 
 /**
  * Class LoadBaseCostData
@@ -26,7 +27,9 @@ class LoadBaseCostData implements FixtureInterface
             ->setDiscountPercent(5)
             ->setEntranceFee(500)
             ->setPaymentCashReward(5)
-            ->setPaymentCreditReward(5);
+            ->setPaymentCreditReward(5)
+            ->setLocation(new Point(59.605560, 36.298468))
+            ->setLocationRadius(10000);
 
         $manager->persist($cost);
         $manager->flush();
