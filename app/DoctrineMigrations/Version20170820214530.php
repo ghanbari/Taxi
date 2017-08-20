a30cea63f06b703ee2a867a514a249b3bc600b88
Ghanbari <ghanbari.daniel@gmail.com>
2017-08-20 21:48:54 +0430

    data migrate

---
+++ app/DoctrineMigrations/Version20170820214530.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Itaxi\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170820214530 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        
+        $this->addSql('DROP INDEX plaque_UNIQUE ON plaque');
+        $this->addSql('ALTER TABLE discount_code CHANGE code code VARCHAR(50) NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        
+        $this->addSql('ALTER TABLE discount_code CHANGE code code VARCHAR(10) NOT NULL COLLATE utf8_unicode_ci');
+        $this->addSql('CREATE UNIQUE INDEX plaque_UNIQUE ON plaque (first_number, second_number, city_number, area_code)');
+    }
+}
